75bb8bd17fb80d1fe8846b90c1715de94c5ff846
Vijay Jha <mr.vijayjha@gmail.com>
2023-07-11 14:51:59 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/module2.mjs
@@ -0,0 +1,7 @@
+const a = "Harry";
+const b = "Rohan";
+const c = "Akash";
+const d = "Priyanka";
+
+export default b;
+export {a};
